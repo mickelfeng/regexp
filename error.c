e76c602b4a0aef113a894d0a259dba0e0da9e3a8
julp <julp@redaction-developpez.com>
2011-07-31 23:08:13 +0200

    allow "chaining"

--- error.c
+++ error.c
@@ -17,14 +17,20 @@ void intl_errors_setf_custom_msg(intl_error *err TSRMLS_DC, char *format, ...)
     intl_errors_set_custom_msg(err, msg, FALSE TSRMLS_CC);
 }
 
-void intl_error_non_quiet_set_code(UErrorCode err_code TSRMLS_DC)
+int intl_error_non_quiet_set_code(UErrorCode err_code TSRMLS_DC)
 {
     intl_error *err;
 
     if (NULL != (err = intl_g_error_get(TSRMLS_C))) {
         err->code = err_code;
-        if (U_FAILURE(err_code) && 0 != INTL_G(error_level)) {
-            php_error_docref(NULL TSRMLS_CC, INTL_G(error_level), "%s(%d)", u_errorName(err_code), err_code);
+        if (U_FAILURE(err_code)) {
+            if (0 != INTL_G(error_level)) {
+                php_error_docref(NULL TSRMLS_CC, INTL_G(error_level), "%s(%d)", u_errorName(err_code), err_code);
+            }
+            return FALSE;
         }
+        return TRUE;
     }
+
+    return U_SUCCESS(err_code);
 }
--- error.h
+++ error.h
@@ -7,6 +7,6 @@
 # include <unicode/utypes.h>
 
 void intl_errors_setf_custom_msg(intl_error * TSRMLS_DC, char *, ...);
-void intl_error_non_quiet_set_code(UErrorCode TSRMLS_DC);
+int intl_error_non_quiet_set_code(UErrorCode TSRMLS_DC);
 
 #endif /* !INTL_EXTENDED_ERROR_H */
